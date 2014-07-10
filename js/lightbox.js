4878b4924050ec1ec569602c37942c5a1b402266
Hwan Kim <ihwan01@gmail.com>
2014-07-10 10:34:40 -0700

    image container by width
    It is for text readable image.
    
    Originally, lightbox2 resizing image function depends on screen height.
    But if it has text it makes hard to read. This patch, it makes to
    resize a image depends on screen width.
    
    There is 3 option. You should turn off default fitImagesInViewport and
    turn on fitImagesInViewportByWidth. You need to specify the max width.
    
    this.fitImagesInViewport          = false;
    this.fitImagesInViewportByWidth   = true;
    this.containerMaxWidth            = 1140;

--- js/lightbox.js
+++ js/lightbox.js
@@ -5,6 +5,8 @@
  * @license http://creativecommons.org/licenses/by/2.5/
  * - Free for use in both personal and commercial projects
  * - Attribution requires leaving author name, author link, and the license info intact
+ *
+ * Added fitImagesInViewportByWidth function. It is for readable image.
  */
 
 (function() {
@@ -13,15 +15,17 @@
 
   var LightboxOptions = (function() {
     function LightboxOptions() {
-      this.fadeDuration                = 500;
-      this.fitImagesInViewport         = true;
-      this.resizeDuration              = 700;
-      this.positionFromTop             = 50;
-      this.showImageNumberLabel        = true;
-      this.alwaysShowNavOnTouchDevices = false;
-      this.wrapAround                  = false;
+      this.fadeDuration                 = 500;
+      this.fitImagesInViewport          = false;
+      this.fitImagesInViewportByWidth   = true;
+      this.containerMaxWidth            = 1140;
+      this.resizeDuration               = 700;
+      this.positionFromTop              = 50;
+      this.showImageNumberLabel         = true;
+      this.alwaysShowNavOnTouchDevices  = false;
+      this.wrapAround                   = false;
     }
-    
+
     // Change to localize to non-english language
     LightboxOptions.prototype.albumLabel = function(curImageNum, albumSize) {
       return "Image " + curImageNum + " of " + albumSize;
@@ -59,7 +63,7 @@
     Lightbox.prototype.build = function() {
       var self = this;
       $("<div id='lightboxOverlay' class='lightboxOverlay'></div><div id='lightbox' class='lightbox'><div class='lb-outerContainer'><div class='lb-container'><img class='lb-image' src='' /><div class='lb-nav'><a class='lb-prev' href='' ></a><a class='lb-next' href='' ></a></div><div class='lb-loader'><a class='lb-cancel'></a></div></div></div><div class='lb-dataContainer'><div class='lb-data'><div class='lb-details'><span class='lb-caption'></span><span class='lb-number'></span></div><div class='lb-closeContainer'><a class='lb-close'></a></div></div></div></div>").appendTo($('body'));
-      
+
       // Cache jQuery objects
       this.$lightbox       = $('#lightbox');
       this.$overlay        = $('#lightboxOverlay');
@@ -71,7 +75,7 @@
       this.containerRightPadding = parseInt(this.$container.css('padding-right'), 10);
       this.containerBottomPadding = parseInt(this.$container.css('padding-bottom'), 10);
       this.containerLeftPadding = parseInt(this.$container.css('padding-left'), 10);
-      
+
       // Attach event handlers to the newly minted DOM elements
       this.$overlay.hide().on('click', function() {
         self.end();
@@ -166,7 +170,7 @@
           }
         }
       }
-      
+
       // Position Lightbox
       var top  = $window.scrollTop() + this.options.positionFromTop;
       var left = $window.scrollLeft();
@@ -202,7 +206,7 @@
 
         $image.width(preloader.width);
         $image.height(preloader.height);
-        
+
         if (self.options.fitImagesInViewport) {
           // Fit image inside the viewport.
           // Take into account the border around the image and an additional 10px gutter on each side.
@@ -227,6 +231,29 @@
             }
           }
         }
+
+        if (self.options.fitImagesInViewportByWidth) {
+          // Fit image inside the viewport width.
+          // Take into account the border around the image and an additional 10px gutter on each side.
+
+          windowWidth    = $(window).width();
+          windowHeight   = $(window).height();
+          maxImageWidth  = windowWidth - self.containerLeftPadding - self.containerRightPadding - 20;
+          //maxImageHeight = windowHeight - self.containerTopPadding - self.containerBottomPadding - 120;
+
+          // Is there a fitting issue?
+          if ((preloader.width > maxImageWidth)) {
+              if (preloader.width > self.options.containerMaxWidth) {
+                imageWidth = self.options.containerMaxWidth - 20;
+              } else {
+                imageWidth  = maxImageWidth;
+              }
+              imageHeight = "auto";
+
+              $image.width(imageWidth);
+              $image.height(imageHeight);
+          }
+        }
         self.sizeContainer($image.width(), $image.height());
       };
 
@@ -244,12 +271,12 @@
     // Animate the size of the lightbox to fit the image we are showing
     Lightbox.prototype.sizeContainer = function(imageWidth, imageHeight) {
       var self = this;
-      
+
       var oldWidth  = this.$outerContainer.outerWidth();
       var oldHeight = this.$outerContainer.outerHeight();
       var newWidth  = imageWidth + this.containerLeftPadding + this.containerRightPadding;
       var newHeight = imageHeight + this.containerTopPadding + this.containerBottomPadding;
-      
+
       function postResize() {
         self.$lightbox.find('.lb-dataContainer').width(newWidth);
         self.$lightbox.find('.lb-prevLink').height(newHeight);
@@ -273,7 +300,7 @@
     Lightbox.prototype.showImage = function() {
       this.$lightbox.find('.lb-loader').hide();
       this.$lightbox.find('.lb-image').fadeIn('slow');
-    
+
       this.updateNav();
       this.updateDetails();
       this.preloadNeighboringImages();
@@ -330,15 +357,15 @@
             location.href = $(this).attr('href');
           });
       }
-    
+
       if (this.album.length > 1 && this.options.showImageNumberLabel) {
         this.$lightbox.find('.lb-number').text(this.options.albumLabel(this.currentImageIndex + 1, this.album.length)).fadeIn('fast');
       } else {
         this.$lightbox.find('.lb-number').hide();
       }
-    
+
       this.$outerContainer.removeClass('animating');
-    
+
       this.$lightbox.find('.lb-dataContainer').fadeIn(this.options.resizeDuration, function() {
         return self.sizeOverlay();
       });
